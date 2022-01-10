1bce9a3a06fe8695c71a775a4ef4d73b46dd5e78
Mausam Kumar <mausam15171098@gmail.com>
2022-01-10 22:00:31 +0530

    initiated repo

---
+++ src/redux/reducer.js
@@ -0,0 +1,9 @@
+import {actionTypes} from './actionTypes.js'
+
+const initState = {
+
+}
+
+export const reducer = (state=initState,action) => {
+    
+}
---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import {createStore,applyMiddleware,combineReducers} from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
+import {reducer} from './reducer.js'
+
+
+export const store = createStore(combineReducers({reducer}),composeWithDevTools(applyMiddleware(thunk)))
